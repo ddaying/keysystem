e44facf89fd0aa9cdbe3fdf6cecad45e3dde2422
JEONGHUN <ddaying@naver.com>
2021-06-03 18:15:27 +0900

    init

---
+++ src/main/java/com/ddaying/kakaopay/keysystem/KeysystemApplication.java
@@ -0,0 +1,13 @@
+package com.ddaying.kakaopay.keysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeysystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeysystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddaying/kakaopay/keysystem/KeysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddaying.kakaopay.keysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeysystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
